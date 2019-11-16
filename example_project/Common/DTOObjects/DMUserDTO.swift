a36248e65af7a38e48a8d9449692062ee9c052bf
Magdalena <magdalena.skawinska@intive.com>
2019-11-17 00:33:23 +0100

    get data

---
+++ example_project/Common/DTOObjects/DMUserDTO.swift
@@ -0,0 +1,9 @@
+//
+//  DMUserDTO.swift
+//  example_project
+//
+//  Created by magdalena.skawinska on 17/11/2019.
+//  Copyright © 2019 magdalena.skawinska. All rights reserved.
+//
+
+import Foundation
---
+++ example_project/Common/UsersApi.swift
@@ -0,0 +1,9 @@
+//
+//  UsersApi.swift
+//  example_project
+//
+//  Created by magdalena.skawinska on 16/11/2019.
+//  Copyright © 2019 magdalena.skawinska. All rights reserved.
+//
+
+import Foundation
